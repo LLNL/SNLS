1b91a6073b20bf8260bb75e3d92bb24a2a9927f3
Robert Carson <rac428@cornell.edu>
2019-10-11 09:36:15 -0700

    Update SNLS_TrDLDenseG.cxx

--- SNLS_TrDLDenseG.cxx
+++ SNLS_TrDLDenseG.cxx
@@ -1,2 +1,2 @@
-//BLT spits out errors if we dont have a source file.
-//So we're just going to leave this as being empty
+// BLT spits out errors if we dont have a source file.
+// So we're just going to leave this as being empty
