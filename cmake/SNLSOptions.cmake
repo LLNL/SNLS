0f71139c5a9127bc61bcd2e7f611e77ab625d351
Robert Carson <carson16@llnl.gov>
2022-09-21 12:49:24 -0700

    Add experimental support for experimental raja features

--- cmake/SNLSOptions.cmake
+++ cmake/SNLSOptions.cmake
@@ -1,5 +1,8 @@
 option(USE_BATCH_SOLVERS "Build library with batch solvers which require the RAJA Peformance Suite" OFF)
 
+option(USE_EXPT_RAJA "Make use of experimental RAJA features such as the dynamic forall feature.
+                      Only useable when USE_BATCH_SOLVERS option is on." OFF)
+
 option(USE_LAPACK "Build SNLS with LAPACK support" OFF)
 
 option(ENABLE_TESTS "Enable tests" OFF)
--- src/SNLS_device_forall.h
+++ src/SNLS_device_forall.h
@@ -19,13 +19,39 @@
 #include "chai/config.hpp"
 #include "chai/ExecutionSpaces.hpp"
 
-   // Implementation of SNLS's "parallel for" (forall) device/host kernel
-   // interfaces supporting RAJA and sequential backends which is based on
-   // the MFEM_FORALL macro.
-   // An example would be something like:
-   // SNLS_FORALL(i, 0, 50, {var[i] = 0;});
-   
+// Implementation of SNLS's "parallel for" (forall) device/host kernel
+// interfaces supporting RAJA and sequential backends which is based on
+// the MFEM_FORALL macro.
+// An example would be something like:
+// SNLS_FORALL(i, 0, 50, {var[i] = 0;});
+
+#ifdef SNLS_RAJA_EXPT_FEATURES
+
+using snls_policy_list = camp::list<RAJA::loop_exec
+#if defined(RAJA_ENABLE_OPENMP)
+                               ,RAJA::omp_parallel_for_exec
+#endif
+#if defined(RAJA_ENABLE_CUDA)
+                               ,RAJA::cuda_exec<SNLS_GPU_THREADS>
+#endif
+                               >;
+  
 /// The SNLS_FORALL wrapper where GPU threads are set to a default value
+#define SNLS_FORALL(i, st, end, ...)            \
+snls::SNLS_ForallWrap<SNLS_GPU_THREADS, false>(	\
+st,                                             \
+end,                                            \
+[=] __snls_hdev__ (int i) {__VA_ARGS__})
+
+/// The SNLS_FORALL wrapper that allows one to change the number of GPU threads
+#define SNLS_FORALL_T(i, threads, st, end, ...)  \
+snls::SNLS_ForallWrap<threads, true>(			    \
+st,                                              \
+end,                                             \
+[=] __snls_hdev__ (int i) {__VA_ARGS__})
+
+#else
+
 #define SNLS_FORALL(i, st, end, ...)           \
 snls::SNLS_ForallWrap<SNLS_GPU_THREADS>(		  \
 st,                                            \
@@ -40,6 +66,8 @@ st,                                              \
 end,                                             \
 [=] __snls_device__ (int i) {__VA_ARGS__},       \
 [=] (int i) {__VA_ARGS__})
+
+#endif
 
 // offset a vector / matrix to correct starting memory location given:
 // what local element we're on, what offset that local element is from the global 0,
@@ -105,9 +133,73 @@ namespace snls {
             }
          }
 
+         static inline int GetPolicyNumber() {
+            int pol_id = 0;
+            if (Get()._es == ExecutionStrategy::CPU) {
+               return pol_id;
+            }
+            pol_id++;
+#ifdef OPENMP_ENABLE
+            if (Get()._es == ExecutionStrategy::OPENMP) {
+               return pol_id;
+            }
+            pol_id++;
+#endif
+#ifdef __CUDACC__
+            if (Get()._es == ExecutionStrategy::CUDA) {
+               return pol_id;
+            }
+#endif
+            return pol_id;
+         }
+
          ~Device() {}
    };
 
+
+
+#ifdef SNLS_RAJA_EXPT_FEATURES
+   /// The forall kernel body wrapper. It should be noted that one
+   /// limitation of this wrapper is that the lambda captures can
+   /// only capture functions / variables that are publicly available
+   /// if this is called within a class object.
+   template <const int NUMTHREADS, const bool GPU_THREADS, 
+             typename HDBODY>
+   inline void SNLS_ForallWrap(const int st,
+                               const int end,
+                               HDBODY &&hd_body)
+   {
+      // Additional backends can be added as seen within the MFEM_FORALL
+      // which this was based on.
+      
+      // Device::Backend makes use of a global variable
+      // so as long as this is set in one central location
+      // and you don't have multiple Device objects changing
+      // the backend things should just work no matter where this
+      // is used.
+      if (GPU_THREADS) {
+         switch(Device::GetBackend()) {
+   #ifdef RAJA_ENABLE_CUDA
+            case(ExecutionStrategy::CUDA): {
+               RAJA::forall<RAJA::cuda_exec<NUMTHREADS>>(RAJA::RangeSegment(st, end), hd_body);
+               break;
+            }
+   #endif
+            default: {
+               // Moved from a for loop to raja forall so that the chai ManagedArray
+               // would automatically move the memory over
+               const int pol_id = Device::GetPolicyNumber();
+               RAJA::expt::dynamic_forall<snls_policy_list>(pol_id, RAJA::RangeSegment(st, end), hd_body);
+               break;
+            }
+         } // End of switch
+      }
+      else {
+         const int pol_id = Device::GetPolicyNumber();
+         RAJA::expt::dynamic_forall<snls_policy_list>(pol_id, RAJA::RangeSegment(st, end), hd_body);
+      }
+   } // end of forall wrap
+#else
    /// The forall kernel body wrapper. It should be noted that one
    /// limitation of this wrapper is that the lambda captures can
    /// only capture functions / variables that are publically available
@@ -148,6 +240,7 @@ namespace snls {
          }
       } // End of switch
    } // end of forall wrap
+#endif // SNLS_RAJA_EXPT_FEATURES
 }
 #endif // SNLS_RAJA_PERF_SUITE
 #endif /* SNLS_device_forall_h */
