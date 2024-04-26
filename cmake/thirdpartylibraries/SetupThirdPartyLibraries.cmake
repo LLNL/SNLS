79d0411c4ed886613f8c1bc916c38e2023cc51a0
Robert Carson <carson16@llnl.gov>
2024-04-26 10:23:36 -0700

    Attempt at updating cmake find package logic

--- cmake/thirdpartylibraries/SetupThirdPartyLibraries.cmake
+++ cmake/thirdpartylibraries/SetupThirdPartyLibraries.cmake
@@ -16,19 +16,31 @@ endforeach()
 # Only search for these if the batch solver is enabled
 if(USE_BATCH_SOLVERS)
 
+message("Use SNLS batch solvers...")
+
 ################################
 # camp
 ################################
 
 if (CAMP_DIR)
-    include(cmake/thirdpartylibraries/Findcamp.cmake)
-    if (CAMP_FOUND)
-        blt_register_library( NAME       camp
-                              TREAT_INCLUDES_AS_SYSTEM ON
-                              INCLUDES   ${CAMP_INCLUDE_DIRS})
-    else()
-        message(FATAL_ERROR "Unable to find camp with given path ${CAMP_DIR}")
-    endif()
+   find_package(camp REQUIRED CONFIG PATHS ${CAMP_DIR})
+   find_package_handle_standard_args(camp REQUIRED)
+   message("camp found?")
+else()
+    message(FATAL_ERROR "CAMP_DIR was not provided. It is needed to find CAMP.")
+endif()
+
+
+################################
+# chai
+################################
+
+if (DEFINED CHAI_DIR)
+   find_package(chai REQUIRED CONFIG PATHS ${CHAI_DIR})
+   find_package_handle_standard_args(chai REQUIRED)
+   message("chai found?")
+else()
+    message(FATAL_ERROR "CHAI_DIR was not provided. It is needed to find CHAI.")
 endif()
 
 ################################
@@ -36,17 +48,9 @@ endif()
 ################################
 
 if (DEFINED RAJA_DIR)
-    include(cmake/thirdpartylibraries/FindRAJA.cmake)
-    if (RAJA_FOUND)
-        blt_register_library( NAME       raja
-                              TREAT_INCLUDES_AS_SYSTEM ON
-                              INCLUDES   ${RAJA_INCLUDE_DIRS}
-                              DEPENDS_ON ${RAJA_DEPENDS}
-                              LIBRARIES  ${RAJA_LIBRARIES}
-                              DEFINES    HAVE_RAJA)
-    else()
-        message(FATAL_ERROR "Unable to find RAJA with given path ${RAJA_DIR}")
-    endif()
+   find_package(RAJA REQUIRED CONFIG PATHS ${RAJA_DIR})
+   find_package_handle_standard_args(RAJA REQUIRED)
+   message("raja found?")
 else()
     message(FATAL_ERROR "RAJA_DIR was not provided. It is needed to find RAJA.")
 endif()
@@ -57,39 +61,11 @@ endif()
 ################################
 
 if (DEFINED UMPIRE_DIR)
-    include(cmake/thirdpartylibraries/FindUmpire.cmake)
-    if (UMPIRE_FOUND)
-        blt_register_library( NAME       umpire
-                              TREAT_INCLUDES_AS_SYSTEM ON
-                              INCLUDES   ${UMPIRE_INCLUDE_DIRS}
-                              DEPENDS_ON ${UMPIRE_DEPENDS}
-                              LIBRARIES  ${UMPIRE_LIBRARIES}
-                              DEFINES    HAVE_UMPIRE)
-    else()
-        message(FATAL_ERROR "Unable to find UMPIRE with given path ${UMPIRE_DIR}")
-    endif()
+   find_package(umpire REQUIRED CONFIG PATHS ${UMPIRE_DIR})
+   find_package_handle_standard_args(umpire REQUIRED)
+   message("umpire found?")
 else()
     message(FATAL_ERROR "UMPIRE_DIR was not provided. It is needed to find UMPIRE.")
-endif()
-
-################################
-# CHAI
-################################
-
-if (DEFINED CHAI_DIR)
-    include(cmake/thirdpartylibraries/FindCHAI.cmake)
-    if (CHAI_FOUND)
-        blt_register_library( NAME       chai
-                              TREAT_INCLUDES_AS_SYSTEM ON
-                              INCLUDES   ${CHAI_INCLUDE_DIRS}
-                              DEPENDS_ON ${CHAI_DEPENDS}
-                              LIBRARIES  ${CHAI_LIBRARIES}
-                              DEFINES    HAVE_CHAI)
-    else()
-        message(FATAL_ERROR "Unable to find CHAI with given path ${CHAI_DIR}")
-    endif()
-else()
-    message(FATAL_ERROR "CHAI_DIR was not provided. It is needed to find CHAI.")
 endif()
 
 endif() # end of enable batch solvers
