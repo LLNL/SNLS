609f96ff5b6b9d13ff878b427e6a3c4223f1720e
Alan Dayton <dayton8@llnl.gov>
2026-06-29 13:23:34 -0700

    More dependency management in SNLS

--- cmake/thirdpartylibraries/SetupThirdPartyLibraries.cmake
+++ cmake/thirdpartylibraries/SetupThirdPartyLibraries.cmake
@@ -16,73 +16,25 @@ endforeach()
 # Only search for these if the batch solver is enabled
 
 if(USE_RAJA_ONLY OR USE_BATCH_SOLVERS)
+  if(NOT TARGET camp)
+    find_package(camp REQUIRED CONFIG NO_DEFAULT_PATH PATHS ${CAMP_DIR})
+  endif()
 
-################################
-# camp
-################################
-
-if (CAMP_DIR)
-   find_package(camp REQUIRED CONFIG PATHS ${CAMP_DIR})
-elseif(USE_BATCH_SOLVERS)
-   message(FATAL_ERROR "CAMP_DIR was not provided. It is needed to find CAMP.")
-endif()
-
-################################
-# RAJA
-################################
-
-if (RAJA_DIR)
-   if (CAMP_DIR AND TARGET camp)
-      set(camp_DIR ${CAMP_DIR})
-   elseif(USE_BATCH_SOLVERS)
-     message(FATAL_ERROR "RAJA is enabled but camp target not found")
-   endif()
-   find_package(RAJA REQUIRED CONFIG PATHS ${RAJA_DIR})
-else()
-   message(FATAL_ERROR "RAJA_DIR was not provided. It is needed to find RAJA.")
-endif()
-
+  if(NOT TARGET RAJA)
+    find_package(raja REQUIRED CONFIG NO_DEFAULT_PATH PATHS ${RAJA_DIR})
+  endif()
 endif()
 
 if(USE_BATCH_SOLVERS)
+  if(NOT TARGET fmt::fmt and NOT TARGET fmt::fmt-header-only)
+    find_package(fmt REQUIRED CONFIG NO_DEFAULT_PATH PATHS ${FMT_DIR})
+  endif()
 
-################################
-# chai
-################################
+  if(NOT TARGET umpire)
+    find_package(umpire REQUIRED CONFIG NO_DEFAULT_PATH PATHS ${UMPIRE_DIR})
+  endif()
 
-if (CHAI_DIR)
-   if (FMT_DIR)
-     set(fmt_DIR ${FMT_DIR})
-   endif()
-   if (UMPIRE_DIR AND RAJA_DIR)
-      set(umpire_DIR ${UMPIRE_DIR})
-      set(raja_DIR ${RAJA_DIR})
-   else()
-      message(FATAL_ERROR "CHAI is enabled but raja and/or umpire directories are not defined")
-   endif()  
-   find_package(chai REQUIRED CONFIG PATHS ${CHAI_DIR})
-else()
-   message(FATAL_ERROR "CHAI_DIR was not provided. It is needed to find CHAI.")
+  if(NOT TARGET chai)
+    find_package(chai REQUIRED CONFIG NO_DEFAULT_PATH PATHS ${CHAI_DIR})
+  endif()
 endif()
-
-################################
-# fmt
-################################
-
-if (FMT_DIR)
-   find_package(fmt CONFIG PATHS ${FMT_DIR})
-else()
-   message(WARNING "FMT_DIR was not provided. This is a requirement for camp as of v2024.02.0. Ignore this warning if using older versions of the RAJA Portability Suite")
-endif()
-
-################################
-# UMPIRE
-################################
-
-if (DEFINED UMPIRE_DIR)
-   find_package(umpire REQUIRED CONFIG PATHS ${UMPIRE_DIR})
-else()
-   message(FATAL_ERROR "UMPIRE_DIR was not provided. It is needed to find UMPIRE.")
-endif()
-
-endif() # end of enable batch solvers
